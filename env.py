aa8ad0be8fa8e78782fb127916719ace3e83ed3f
reda-maizate <reda-maizate@estiam.com>
2021-05-22 19:37:34 +0200

    changed the path of the notebook to facilitate the importation

--- env.py
+++ env.py
@@ -1,1 +1,1 @@
-DLL_PATH="C:/Users/ttres/Desktop/3A/S2/MachineLearning/ml_library/cmake-build-debug/ml_library.dll"
+DLL_PATH = "/Users/redamaizate/Documents/3IABD/Projet-Annuel/ml_library/cmake-build-debug/libml_library.dylib"
