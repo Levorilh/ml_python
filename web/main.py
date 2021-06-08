4c53b6406241febe094910f04218e8e329550650
Thomas Tresgots <ttresgots@gmail.com>
2021-06-08 16:03:07 +0200

    web implem started

--- web/main.py
+++ web/main.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
+
+MONUMENTS = ["Tour eiffel", "Arc de Triomphe", "Notre dame", "Inconnu"]
 
 
 @app.route("/")
 def hello_world():
     return render_template('home.twig')
+
+
+@app.route("/predict", methods=["POST"])
+def predict_route():
+    className = MONUMENTS[random.randint(0, len(MONUMENTS)-1)]
+
+    accuracy = 60 + random.random()*40
+    return render_template('prediction.twig', className=className, accuracy=accuracy)
