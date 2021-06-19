29e2cc51ca2d7efe82f583ff7f8eaa9b44a0e246
Thomas Tresgots <ttresgots@gmail.com>
2021-06-20 00:37:46 +0200

    add request and session options from flask, new route setModel

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import random
 
 app = Flask(__name__)
@@ -8,7 +8,12 @@ MONUMENTS = ["Tour eiffel", "Arc de Triomphe", "Notre dame", "Inconnu"]
 
 @app.route("/")
 def hello_world():
-    return render_template('home.twig')
+    modelIds = {
+        "LINEAR": [41, 31, 21, 11],
+        "MLP": [4, 3, 2, 1],
+    }
+
+    return render_template('home.twig', modelIds=modelIds)
 
 
 @app.route("/predict", methods=["POST"])
@@ -19,5 +24,13 @@ def predict_route():
     return render_template('prediction.twig', className=className, accuracy=accuracy)
 
 
+@app.route("/setModel", methods=["POST"])
+def setModel_route():
+    session['model'] = request.form["id"]
+
+    return "",201
+
+
 if __name__ == '__main__':
+    app.secret_key = "3A-IABD2"
     app.run(debug=True)
