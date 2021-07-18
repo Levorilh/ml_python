b81cacf21307557020316a43ac6950652fd3d0b6
reda-maizate <reda-maizate@estiam.com>
2021-07-18 23:47:23 +0200

    report almost finished...

--- rbfn.py
+++ rbfn.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ctypes import *
 from settings import *
+
 """
 class Centroid(Structure):
     _fields_ = [
@@ -20,23 +21,28 @@ class RBF(Structure):
         ("clusters", POINTER(POINTER(c_void_p)))
     ]
 """
+
+
 def as_C_array(dataset, datatype=c_float):
     arr_size = len(dataset)
     arr_type = datatype * arr_size
     arr = arr_type(*dataset)
     return arr, arr_type
 
-def create_rbfn_model(input_dim, num_classes, k) :
+
+def create_rbfn_model(input_dim, num_classes, k):
     mylib.create_rbfn_model.argtypes = [c_int, c_int, c_int]
     mylib.create_rbfn_model.restype = c_void_p
     return mylib.create_rbfn_model(input_dim, num_classes, k)
 
-def destroy_rbfn_model(model) :
+
+def destroy_rbfn_model(model):
     mylib.destroy_rbfn_model.argtypes = [c_void_p]
     mylib.destroy_rbfn_model.restype = None
     mylib.destroy_rbfn_model(model)
 
-def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, max_iters = 100) :
+
+def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, max_iters=100):
     samples_count = len(dataset_inputs)
     flattened_dataset_inputs = np.array(dataset_inputs).flatten()
     flattened_dataset_expected_outputs = np.array(dataset_expected_outputs).flatten()
@@ -46,7 +52,8 @@ def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, max_iters 
     mylib.train_rbfn_model.restype = None
     mylib.train_rbfn_model(model, arr_input, samples_count, arr_output, max_iters)
 
-def predict_rbfn(model, dataset_inputs) :
+
+def predict_rbfn(model, dataset_inputs):
     flattened_dataset_inputs = np.array(dataset_inputs).flatten()
     arr, arr_type = as_C_array(flattened_dataset_inputs, c_double)
     mylib.predict_rbfn.argtypes = [c_void_p, arr_type]
@@ -56,7 +63,8 @@ def predict_rbfn(model, dataset_inputs) :
     destroy_rbfn_prediction(predict_value)
     return rslt
 
-def destroy_rbfn_prediction(prediction) :
+
+def destroy_rbfn_prediction(prediction):
     mylib.destroy_rbfn_prediction.argtypes = [POINTER(c_double)]
     mylib.destroy_rbfn_prediction.restype = None
     mylib.destroy_rbfn_prediction(prediction)
