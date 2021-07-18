75e5e3970977fd79e9ca05dd3c3f9abd7f1b787b
Horville <59647345+Horville@users.noreply.github.com>
2021-07-18 21:00:10 +0200

    test rbfn

--- rbfn.py
+++ rbfn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ctypes import *
 from settings import *
-
+"""
 class Centroid(Structure):
     _fields_ = [
         ("label",c_int),
@@ -16,10 +16,10 @@ class RBF(Structure):
         ("num_classes", c_int),
         ("k", c_int),
         ("samples_count", c_int),
-        ("W", POINTER(POINTER(c_float))),
-        ("clusters", POINTER(POINTER(Centroid)))
+        ("W", POINTER(POINTER(c_double))),
+        ("clusters", POINTER(POINTER(c_void_p)))
     ]
-
+"""
 def as_C_array(dataset, datatype=c_float):
     arr_size = len(dataset)
     arr_type = datatype * arr_size
@@ -40,21 +40,23 @@ def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, max_iters 
     samples_count = len(dataset_inputs)
     flattened_dataset_inputs = np.array(dataset_inputs).flatten()
     flattened_dataset_expected_outputs = np.array(dataset_expected_outputs).flatten()
-    arr_input, input_type = as_C_array(flattened_dataset_inputs)
-    arr_output, output_type = as_C_array(flattened_dataset_expected_outputs)
+    arr_input, input_type = as_C_array(flattened_dataset_inputs, c_double)
+    arr_output, output_type = as_C_array(flattened_dataset_expected_outputs, c_double)
     mylib.train_rbfn_model.argtypes = [c_void_p, input_type, c_int, output_type, c_int]
     mylib.train_rbfn_model.restype = None
     mylib.train_rbfn_model(model, arr_input, samples_count, arr_output, max_iters)
 
 def predict_rbfn(model, dataset_inputs) :
     flattened_dataset_inputs = np.array(dataset_inputs).flatten()
-    arr, arr_type = as_C_array(flattened_dataset_inputs)
+    arr, arr_type = as_C_array(flattened_dataset_inputs, c_double)
     mylib.predict_rbfn.argtypes = [c_void_p, arr_type]
-    mylib.predict_rbfn.restype = POINTER(c_float)
-    return mylib.predict_rbfn(model, arr)
+    mylib.predict_rbfn.restype = POINTER(c_double)
+    predict_value = mylib.predict_rbfn(model, arr)
+    rslt = list(np.ctypeslib.as_array(predict_value, (2,)))
+    destroy_rbfn_prediction(predict_value)
+    return rslt
 
 def destroy_rbfn_prediction(prediction) :
-    arr, arr_type = as_C_array(prediction)
-    mylib.destroy_rbfn_prediction.argtypes = [arr_type]
+    mylib.destroy_rbfn_prediction.argtypes = [POINTER(c_double)]
     mylib.destroy_rbfn_prediction.restype = None
-    mylib.destroy_rbfn_prediction(arr)
+    mylib.destroy_rbfn_prediction(prediction)
