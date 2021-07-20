5606ba2b4dfa66625086fc8cf64b9065cddfe5b4
Horville <59647345+Horville@users.noreply.github.com>
2021-07-20 17:30:02 +0200

    rbf xor dans cas_de_test

--- rbfn.py
+++ rbfn.py
@@ -2,27 +2,6 @@ import numpy as np
 from ctypes import *
 from settings import *
 
-"""
-class Centroid(Structure):
-    _fields_ = [
-        ("label",c_int),
-        ("coords",POINTER(c_double)),
-        ("coord_count",c_int),
-        ("deviation",c_double)
-    ]
-
-class RBF(Structure):
-    _fields_ = [
-        ("input_dim", c_int),
-        ("num_classes", c_int),
-        ("k", c_int),
-        ("samples_count", c_int),
-        ("W", POINTER(POINTER(c_double))),
-        ("clusters", POINTER(POINTER(c_void_p)))
-    ]
-"""
-
-
 def as_C_array(dataset, datatype=c_float):
     arr_size = len(dataset)
     arr_type = datatype * arr_size
@@ -42,15 +21,15 @@ def destroy_rbfn_model(model):
     mylib.destroy_rbfn_model(model)
 
 
-def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, max_iters=100):
+def train_rbfn_model(model, dataset_inputs, dataset_expected_outputs, naif=True, max_iters=100):
     samples_count = len(dataset_inputs)
     flattened_dataset_inputs = np.array(dataset_inputs).flatten()
     flattened_dataset_expected_outputs = np.array(dataset_expected_outputs).flatten()
     arr_input, input_type = as_C_array(flattened_dataset_inputs, c_double)
     arr_output, output_type = as_C_array(flattened_dataset_expected_outputs, c_double)
-    mylib.train_rbfn_model.argtypes = [c_void_p, input_type, c_int, output_type, c_int]
+    mylib.train_rbfn_model.argtypes = [c_void_p, input_type, c_int, output_type, c_bool, c_int]
     mylib.train_rbfn_model.restype = None
-    mylib.train_rbfn_model(model, arr_input, samples_count, arr_output, max_iters)
+    mylib.train_rbfn_model(model, arr_input, samples_count, arr_output, naif, max_iters)
 
 
 def predict_rbfn(model, dataset_inputs):
