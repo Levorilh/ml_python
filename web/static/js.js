5af77b6a0cb24455853a99e026bea5e3b0cca66e
Thomas Tresgots <ttresgots@gmail.com>
2021-07-27 00:01:19 +0200

    complete rebuild of web app

--- web/static/js.js
+++ web/static/js.js
@@ -4,12 +4,14 @@ let app = {
 };
 
 const MONUMENTS = ["Tour eiffel", "Arc de Triomphe", "Notre dame"];
-$("#insert").on("change", function () {
+
+let $imageForm = $("#imageForm");
+
+$imageForm.on("change", function () {
     const [file] = this.files;
     const $preview = $("#preview");
     if (file) {
         $preview.attr("src", URL.createObjectURL(file));
-        $('#test').removeClass('d-none');
         $preview.removeClass('d-none');
 
     } else {
@@ -40,14 +42,33 @@ $(".modelLoader").on("click", function () {
 });
 
 
+$("#sendImage").on("click" , function (){
+    if (checkInputs()){
+
+        let form = new FormData($("form")[1]);
+
+        console.log(form);
+        $.ajax({
+            url: "/predictImage",
+            data: form,
+            method: "POST",
+            dataType: "html",
+            success: function (html){
+                $("#result").html(html);
+            },
+            processData: false, // important
+            contentType: false, // important
+        });
+    }
+})
+
 function checkInputs() {
-    // console.log(($("#imageForm").files));
     if (!app.hasModel) {
         window.alert("Vous devez d'abord charger un modèle");
         return false;
     }
 
-    if ($("#imageForm")[0].files.length === 0) {
+    if ($imageForm[0].files.length === 0) {
         window.alert("Vous devez insérer une image");
         return false;
 
